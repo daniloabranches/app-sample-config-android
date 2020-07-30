59f3e7143e53f9969afe7cb0fe4ad15f97c64cc8
Danilo Abranches <daniloeabranches@gmail.com>
2020-07-30 14:53:42 -0300

    Melhorias no código Inclusão de chamada para abrir tela de dados móveis correta

--- app/src/main/java/com/test/configurations/MainActivity.kt
+++ app/src/main/java/com/test/configurations/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.test.configurations
 
+import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
@@ -15,35 +16,52 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnChangeDatetime.setOnClickListener {
-            openSettingsAndroid(
-                Settings.ACTION_DATE_SETTINGS,
-                R.string.message_error_config_datetime
-            )
+            openDateSettings()
         }
 
         btnChangeStatus4G.setOnClickListener {
-            openSettingsAndroid(
-                Settings.ACTION_DATA_ROAMING_SETTINGS,
-                R.string.message_error_config_mobile_data
-            )
+            openDataUsageSettings()
         }
 
         btnChangeKeyboard.setOnClickListener {
-            val imeManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-            imeManager.showInputMethodPicker()
+            openKeyboardSettings()
         }
     }
 
-    private fun openSettingsAndroid(settings: String, resIdMessageError: Int) {
-        val intent = Intent(settings)
+    private fun openDateSettings() {
+        val intent = Intent(Settings.ACTION_DATE_SETTINGS)
         if (intent.resolveActivity(packageManager) != null) {
             startActivity(intent)
         } else {
             Toast.makeText(
                 this,
-                getString(resIdMessageError),
+                getString(R.string.message_error_config_datetime),
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+    private fun openDataUsageSettings() {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.component = ComponentName(
+            "com.android.settings",
+            "com.android.settings.Settings\$DataUsageSummaryActivity"
+        )
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(
+                this,
+                getString(R.string.message_error_config_mobile_data),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun openKeyboardSettings() {
+        val imeManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imeManager.showInputMethodPicker()
     }
 }
